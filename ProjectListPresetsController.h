f5f52df3edf29851357ffa3961210c77cf2125e7
Daniel Hahn <dha@betterplace.org>
2011-03-18 11:50:19 +0100

    Some fixes, starting to implement new selector

---
+++ ProjectListPresetsController.h
@@ -0,0 +1,16 @@
+//
+//  ProjectListPresetsController.h
+//  api_curly
+//
+//  Created by Daniel Hahn on 11.03.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ProjectListPresetsController : NSObject {
+
+}
+
+@end
---
+++ ProjectListPresetsController.m
@@ -0,0 +1,14 @@
+//
+//  ProjectListPresetsController.m
+//  api_curly
+//
+//  Created by Daniel Hahn on 11.03.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ProjectListPresetsController.h"
+
+
+@implementation ProjectListPresetsController
+
+@end
